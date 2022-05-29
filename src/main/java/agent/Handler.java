99708aba68476ceda72e7f092ef247a6617d8ddb
晓斌 <xiaobin@xiaobindeMacBook-Air.local>
2022-05-28 23:31:39 -0700

    add sql statement

--- src/main/java/agent/Handler.java
+++ src/main/java/agent/Handler.java
@@ -3,14 +3,25 @@ package agent;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 class Handler implements Callable<String> {
     Socket sock;
+    private String ReadSQL;
+    private String WriteSQL;
 
     public Handler(Socket sock) {
         this.sock = sock;
+        this.ReadSQL = "select object_value from table_test where object_name = ?";
+        this.WriteSQL = "If exists (select 1 from table_test where object_name = ?)\n" +
+                "Update table_test\n" +
+                "Set ?\n" +
+                "Where object_name = ?;\n" +
+                "Else\n" +
+                "Insert into table_test\n" +
+                "values (?, ?)\n";
     }
 
     @Override
@@ -33,18 +44,21 @@ class Handler implements Callable<String> {
     private String handle(InputStream input, OutputStream output) throws IOException {
         var writer = new BufferedWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8));
         var reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
-//            writer.write("hello\n");
-//            writer.flush();
-//            for (;;) {
-//                String s = reader.readLine();
-//                if (s.equals("bye")) {
-//                    writer.write("bye\n");
-//                    writer.flush();
-//                    break;
-//                }
-//                writer.write("ok: " + s + "\n");
-//                writer.flush();
-//            }
-        return reader.readLine();
+            for (;;) {
+                String s = reader.readLine();
+                if(s.equals("end")){
+                    break;
+                }
+                transfer(s);
+                writer.write("ok: " + s + "\n");
+                writer.flush();
+            }
+        return "ok";
     }
+
+    private void transfer(String str){
+
+    }
+
+
 }
